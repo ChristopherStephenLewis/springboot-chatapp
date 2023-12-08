eed29f09d8f8475ebe4ab8e95cabdd7122cf6748
Christopher Lewis <chris.stephen.lewis@gmail.com>
2023-12-08 14:57:52 -0500

    Initial commit

---
+++ src/test/java/com/lewis/springBootChatApp/SpringBootChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lewis.springBootChatApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootChatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
